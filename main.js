6ee070de175eb09f29a21ba57ab4f427a44ba9fb
Your Name <smile2sasi95@gmail.com>
2023-03-27 09:07:54 +0530

    add

---
+++ main.js
@@ -0,0 +1,10 @@
+let n=parseInt(prompt("Enter a value"));
+			function on(n){
+			for(i=0; i<=n; i++){
+				if(i%2==1){
+					document.write(i);
+					document.write("<br>");
+				}
+			}
+			}
+			on(n);
